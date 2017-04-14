bb5308e29354f1ff758ff8ae04fac43280fddc9b
waden10 <fantm10@hotmail.com>
2017-04-14 04:58:15 +0000

    login page

---
+++ users.js
@@ -0,0 +1,13 @@
+const users = [ 
+ {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+];
+module.exports = users;
